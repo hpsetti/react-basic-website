908a0fdafae185b15771b85827d644f66e22545e
Harish Pillarisetti <44186210+hpsetti@users.noreply.github.com>
2021-03-18 02:12:53 +0530

    Added local files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./components/Pages/Home"
+import Products from './components/Pages/Products';
+import Services from './components/Pages/Services';
+import SignUp from './components/Pages/SignUp';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+            
+          <Route path="/services" component={Services}/>
+          <Route path="/products" component={Products}/>
+          <Route path="/sign-up" component={SignUp}/>
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
